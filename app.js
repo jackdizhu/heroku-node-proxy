ec8aae2628e262daa5bd309752cc891bb63ce8ec
root1 <root1@jackdizhudeMacBook-Pro.local>
2019-10-16 17:02:01 +0800

    添加debug插件

--- app.js
+++ app.js
@@ -19,33 +19,44 @@ var app = express();
 
 var google_analytics_id = process.env.GA_ID || null;
 
-function addGa(html) {
+function addGa (html) {
     if (google_analytics_id) {
-        var ga = [
-            "<script type=\"text/javascript\">",
-            "var _gaq = []; // overwrite the existing one, if any",
-            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
-            "_gaq.push(['_trackPageview']);",
-            "(function() {",
-            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
-            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
-            "})();",
-            "</script>"
-            ].join("\n");
+        var ga = `
+            <script type="text/javascript">
+            var _gaq = []; // overwrite the existing one, if any
+            _gaq.push(['_setAccount', '" + google_analytics_id + "']);
+            _gaq.push(['_trackPageview']);
+            (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+            </script>`;
+        html = html.replace("</body>", ga + "\n\n</body>");
+    }
+    return html;
+}
+function addDebug (html, debug) {
+    if (debug) {
+        var ga = `
+            <script src="//cdn.bootcss.com/eruda/1.5.8/eruda.js"></script>
+            <script>
+              eruda.init();
+            </script>`;
         html = html.replace("</body>", ga + "\n\n</body>");
     }
     return html;
 }
 
-function googleAnalyticsMiddleware(data) {
+function googleAnalyticsMiddleware (data) {
+    const debug = data.url.indexOf('debug') !== -1 // 添加debug插件
     if (data.contentType == 'text/html') {
-
         // https://nodejs.org/api/stream.html#stream_transform
         data.stream = data.stream.pipe(new Transform({
             decodeStrings: false,
-            transform: function(chunk, encoding, next) {
+            transform: function (chunk, encoding, next) {
                 this.push(addGa(chunk.toString()));
+                this.push(addDebug(chunk.toString(), debug));
                 next();
             }
         }));
@@ -68,7 +79,7 @@ app.use(unblocker(unblockerConfig));
 app.use('/', express.static(__dirname + '/public'));
 
 // this is for users who's form actually submitted due to JS being disabled or whatever
-app.get("/no-js", function(req, res) {
+app.get("/no-js", function (req, res) {
     // grab the "url" parameter from the querystring
     var site = querystring.parse(url.parse(req.url).query).url;
     // and redirect the user to /proxy/url
